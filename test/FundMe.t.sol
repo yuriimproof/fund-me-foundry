14aa2e36d411cc7cb6d3dbbd6ebbf75a3b97979a
Yuri Improof <improofyuri@gmail.com>
2025-04-25 16:22:29 +0300

    feat: add price feed version test to FundMe contract

--- test/FundMe.t.sol
+++ test/FundMe.t.sol
@@ -18,4 +18,9 @@ contract FundMeTest is Test {
   function testOwnerIsMsgSender() public view {
     assertEq(fundMe.i_owner(), address(this));
   }
+
+  function testPriceFeedVersionIsAccurate() public view {
+    uint256 version = fundMe.getVersion();
+    assertEq(version, 4);
+  }
 }
